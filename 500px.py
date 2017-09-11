8bd47cdd562896b51e31031b9bc7b304d6c159a3
Rafael Fantini da Costa <fanta-mnix@users.noreply.github.com>
2017-09-11 13:58:57 -0300

    Add folder to store terms retrieved from scrapers

--- 500px.py
+++ 500px.py
@@ -8,4 +8,4 @@ response = requests.get('https://about.500px.com/terms/')
 soup = bs4.BeautifulSoup(response.content, 'html5lib')
 terms = u'\n'.join(format_element(el) for el in soup.select('div.section.clearfix > div.left'))
 
-write_all('500px.txt', terms)
+write_all('data/500px.txt', terms)
